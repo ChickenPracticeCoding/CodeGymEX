53928fd8cf6e4f6b98df08ccb75281828197ddc6
ChickenPracticeCoding <trungkienhaui03@gmail.com>
2024-11-19 21:31:54 +0700

    Buoi 3
    Buoi 3 TH-C# co ban

---
+++ BTTinhTong/BTTinhTong/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BTTinhTong
+{
+    internal class Program
+    {
+        public static void Tong()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Nhập sô hạng thứ nhất: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Nhập sô hạng thứ hai: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("-----------------------");
+            Console.WriteLine();
+            Console.Write("Tổng 2 số a + b = {0} ", a + b);
+
+        }
+        static void Main(string[] args)
+        {
+            Tong();
+        }
+    }
+}
---
+++ BTTinhTong/DienTicCN/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DienTicCN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            S_Chu_Nhat();
+        }
+        public static void S_Chu_Nhat()
+        {
+            float width;
+            float height;
+            Console.Write("Enter width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            height = float.Parse(Console.ReadLine());
+            float area = width * height;
+            Console.WriteLine("Area is: " + area);
+        }
+    }
+}
