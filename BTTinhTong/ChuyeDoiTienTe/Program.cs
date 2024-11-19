7c26bb5b80cd55f407f970462d5ead7c03935ea5
ChickenPracticeCoding <trungkienhaui03@gmail.com>
2024-11-19 21:34:31 +0700

    Update Program.cs

--- BTTinhTong/ChuyeDoiTienTe/Program.cs
+++ BTTinhTong/ChuyeDoiTienTe/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double rate = 25000; // Tỉ giá VND/USD
+
+            Console.Write("Nhập giá trị USD: ");
+            double usd = Convert.ToDouble(Console.ReadLine()); // Nhập giá trị USD từ người dùng
+
+            double vnd = usd * rate; // Tính giá trị VND tương ứng
+
+            Console.WriteLine($"Giá trị {usd} USD tương đương với {vnd} VND.");
         }
+       
     }
 }
