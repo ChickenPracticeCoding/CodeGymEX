af389b86d5cfa5cade7ceb3855bdf3a18f333a0d
ChickenPracticeCoding <trungkienhaui03@gmail.com>
2024-12-02 00:35:11 +0700

    Code TH về HCN

---
+++ OOPExample/OOPExample/HCN.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExample
+{
+    internal class HCN
+    {
+        public double CDai
+        { get; set; }
+
+        public double CRong
+        { get;set; }
+        
+        public void NHAP()
+        {
+            Console.Write("Nhap chieu dai: ");
+            CDai = int.Parse(Console.ReadLine());
+            Console.Write("Nhap chieu rong: ");
+            CRong = int.Parse(Console.ReadLine());
+        }
+        public HCN(){}
+
+        public HCN(double _CDai, double _CRong)
+        {
+            CDai = _CDai;
+            CRong = _CRong;
+        }
+        public double CHuVi()
+        {
+            return  (CDai + CRong) *2;
+        }
+
+        public double DienTich()
+        {
+            return CDai * CRong;
+        }
+    }
+}
---
+++ OOPExample/OOPExample/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace OOPExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HCN hcn1 = new HCN();
+            hcn1.NHAP();
+            Console.WriteLine("Chu vi hinh chu nhat la: {0}", hcn1.CHuVi());
+            Console.WriteLine("Dien tich hình chu nhat la {0} ", hcn1.DienTich());
+        }
+    }
+}
